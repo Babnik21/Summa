55f7beca8e684c7c142ea00f37148065d9e9f9b6
Babnik21 <jure.babnik.21@gmail.com>
2025-07-15 18:20:18 +0200

    Initial Commit

---
+++ Summa/SummaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaApp.swift
+//  Summa
+//
+//  Created by Jure Babnik on 15. 7. 25.
+//
+
+import SwiftUI
+
+@main
+struct SummaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SummaTests/SummaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SummaTests.swift
+//  SummaTests
+//
+//  Created by Jure Babnik on 15. 7. 25.
+//
+
+import Testing
+@testable import Summa
+
+struct SummaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
